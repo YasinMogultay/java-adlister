fe3d1821ff78796cad81a88ea218d53bf80b18f6
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-13 00:36:40 +0300

    completed

--- src/main/java/GuessServlet.java
+++ src/main/java/GuessServlet.java
@@ -14,7 +14,7 @@ public class GuessServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Random random = new Random();
-        int rand =  random.nextInt(3 - 1) + 1;
+        int rand = random.nextInt(3 - 1) + 1;
         System.out.println("random is " + rand);
         String guess = request.getParameter("guess");
         System.out.println(guess);
