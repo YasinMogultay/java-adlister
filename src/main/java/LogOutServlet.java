6aeeb1d0478127b12d769f23b9c4270a7419c8b4
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-15 12:35:17 +0300

    feat: added WEB-INF for security

--- src/main/java/LogOutServlet.java
+++ src/main/java/LogOutServlet.java
@@ -9,12 +9,12 @@ import java.io.IOException;
 public class LogOutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        req.getSession().invalidate();
-        req.getRequestDispatcher("/logout.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
         resp.sendRedirect("/login");
     }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.sendRedirect("/login");
+//    }
 }
